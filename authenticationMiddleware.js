4659fc46f8aa2e4089dfb9054e9366455da76807
ApexBlitz <154977702+ApexBlitz@users.noreply.github.com>
2023-12-27 13:50:04 +0100

    Create authenticationMiddleware.js

---
+++ authenticationMiddleware.js
@@ -0,0 +1,17 @@
+// middleware/authenticationMiddleware.js
+const jwt = require('jsonwebtoken');
+
+const secretKey = 'secret-key';
+
+const authenticateJWT = (req, res, next) => {
+  const token = req.header('Authorization');
+  if (!token) return res.sendStatus(401);
+
+  jwt.verify(token, secretKey, (err, user) => {
+    if (err) return res.sendStatus(403);
+    req.user = user;
+    next();
+  });
+};
+
+module.exports = authenticateJWT;
